e646878aaa5c2cf711559a9a0d02c5f8c5fe0638
Aitaneuh <ethanb9407@gmail.com>
2026-05-01 00:53:31 +0200

    made proper landing page

--- src/App.vue
+++ src/App.vue
@@ -36,28 +36,76 @@ provide('selectedColor', activeColor)
 
 <template>
   <div class="layout">
-    <h1>Fabcorp</h1>
-    <RainbowSelector @color="select" @hover="hover" />
-    <h2>Nos projets en : {{ names[activeColor] }}</h2>
-    <div class="projects">
-      <Suspense>
-        <Project :key="key" />
-      </Suspense>
+    <div class="hero page">
+      <div class="title-box">
+        <h1>
+          <div class="orange-text">FAB</div>
+          CORP
+        </h1>
+        <hr class="separator" />
+        <p class="quote">"Code is poetry, performance is our passion."</p>
+      </div>
+    </div>
+    <div class="page">
+      <div class="rainbow-select-box">
+        <RainbowSelector @color="select" @hover="hover" />
+        <h2>Nos projets en : {{ names[activeColor] }}</h2>
+        <div class="projects">
+          <Suspense>
+            <Project :key="key" />
+          </Suspense>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.page {
+  height: 100vh;
+}
+.hero {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.title-box {
+  width: min-content;
+  padding: 3rem;
+  transition: all 0.4s ease;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+h1 {
+  margin: 0;
+  text-align: center;
+  font-weight: 800;
+  font-size: 10rem;
+  display: flex;
+  justify-content: center;
+  line-height: 0.9;
+}
+
+.separator {
+  border: 0;
+  border-top: 1px solid white;
+  margin: 1rem 0 2rem 0;
+  width: 40rem;
+}
+
+.quote {
+  margin: 0;
+  font-style: italic;
+  white-space: nowrap;
+}
+
 h2 {
   text-align: center;
   margin-top: 5dvh;
 }
-
-h1 {
-  text-align: center;
-  font-size: 3rem;
-}
-
 .projects {
   width: 100%;
   display: flex;
@@ -66,5 +114,13 @@ h1 {
   flex-wrap: wrap;
   gap: 24px 12px;
   flex-grow: 1;
+}
+
+.orange-text {
+  color: #ff6d1f;
+}
+
+.rainbow-select-box {
+  padding-top: 5rem;
 }
 </style>
