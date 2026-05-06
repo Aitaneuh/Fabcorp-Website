d435d1e1d545637282549e3d8f8397d8255b90ff
Aitaneuh <ethanb9407@gmail.com>
2026-05-06 08:22:51 +0200

    added navbar core

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-// import Navbar from './components/Navbar.vue'
+import Navbar from './components/Navbar.vue'
 </script>
 
 <template>
-  <!-- <Navbar /> -->
+  <Navbar />
 
   <main>
     <router-view />
---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <nav class="navbar">
+        <div class="logo">Fabcorp</div>
+        <ul class="nav-links">
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link to="/about">About</router-link></li>
+        </ul>
+    </nav>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem 2rem;
+    background-color: #111111;
+    color: white;
+}
+
+.nav-links {
+    display: flex;
+    list-style: none;
+    gap: 20px;
+}
+
+.nav-links a {
+    color: white;
+    text-decoration: none;
+}
+
+.router-link-active {
+    border-bottom: 2px solid #ff6d1f;
+}
+</style>
