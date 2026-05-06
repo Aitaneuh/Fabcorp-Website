708b75d9fb28cf73b68460a5dc1728d2e065583c
Aitaneuh <ethanb9407@gmail.com>
2026-05-06 11:08:29 +0200

    created team page

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
+import TeamView from '../views/TeamView.vue'
 
 const routes = [
     { path: '/', name: 'home', component: HomeView },
     { path: '/about', name: 'about', component: AboutView },
+    { path: '/team', name: 'team', component: TeamView },
 ]
 
 const router = createRouter({
