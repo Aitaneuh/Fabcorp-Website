291d0bb360e51bb8f60747c95784cc713ded1dc7
Aitaneuh <ethanb9407@gmail.com>
2026-05-06 07:53:34 +0200

    refactored project structure and prepared for scalability

--- src/App.vue
+++ src/App.vue
@@ -1,125 +1,13 @@
 <script setup lang="ts">
-import { ref, provide } from 'vue'
-
-const key = ref(0)
-const selectedColor = ref('none')
-const activeColor = ref('none')
-
-const names = {
-  red: 'Red',
-  orange: 'Orange',
-  yellow: 'Yellow',
-  green: 'Green',
-  cyan: 'Cyan',
-  blue: 'Blue',
-  pink: 'Rose',
-  violet: 'Violet',
-  white: 'White',
-  black: 'Black',
-}
-
-function select(color: string) {
-  selectedColor.value = color
-  activeColor.value = color
-  key.value++
-}
-
-function hover(color: string) {
-  if (selectedColor.value == 'none') {
-    activeColor.value = color
-    key.value++
-  }
-}
-
-provide('selectedColor', activeColor)
+// You can import your Navbar here
+// import Navbar from './components/Navbar.vue'
 </script>
 
 <template>
-  <div class="layout">
-    <div class="hero page">
-      <div class="title-box">
-        <h1>
-          <div class="orange-text">fab</div>
-          corp<nobr />
-        </h1>
-        <hr />
-        <p class="quote">"Because we can code it"</p>
-      </div>
-    </div>
-    <div class="page">
-      <div class="rainbow-select-box">
-        <RainbowSelector @color="select" @hover="hover" />
-        <h2>Our project in : {{ names[activeColor] }}</h2>
-        <div class="projects hero">
-          <Suspense>
-            <Project :key="key" />
-          </Suspense>
-        </div>
-      </div>
-    </div>
-  </div>
-  <router-view />
+  <!-- Global Navbar goes here so it stays on every page -->
+  <!-- <Navbar /> -->
+
+  <main>
+    <router-view />
+  </main>
 </template>
-
-<style scoped>
-.page {
-  height: 100vh;
-}
-
-.hero {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.title-box {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-h1 {
-  text-transform: uppercase;
-  margin: 0px;
-  text-align: center;
-  font-weight: 800;
-  font-size: 10rem;
-  display: flex;
-  justify-content: center;
-  line-height: 0.9em;
-}
-
-hr {
-  border-top: 1px solid white;
-  margin: 1rem 0px 2rem 0px;
-  width: 40rem;
-}
-
-.quote {
-  margin: 0px;
-  font-style: italic;
-}
-
-h2 {
-  text-align: center;
-  margin-top: 5dvh;
-}
-
-.projects {
-  display: grid;
-  grid-template-columns: repeat(3, 350px);
-  gap: 24px 12px;
-}
-
-.projects div {
-  align-self: center;
-}
-
-.orange-text {
-  color: #ff6d1f;
-}
-
-.rainbow-select-box {
-  padding-top: 5rem;
-}
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
+// import AboutView from '../views/AboutView.vue'
 
 const routes = [
   { path: '/', name: 'home', component: HomeView },
-  { path: '/about', name: 'about', component: AboutView }
+//   { path: '/about', name: 'about', component: AboutView }
 ]
 
 const router = createRouter({
